27c3959fc4682d539acc93720449784bdc56307b
Demeter Csaba <nuador@yahoo.com>
2016-10-26 11:44:07 +0300

    Start

--- Game/Game/main.cpp
+++ Game/Game/main.cpp
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
 void main() {
-	int szam = 0;
-	printf("Heureka!(%d) Csaba\n",szam);
-	int a = 5;
-	int b = a + szam;
-	int c = 8;
-	printf("%d\n",b);
-	printf("%d\n", c);
+	
 }

d0cc72f8d541c095ed4d71a443a40147aa99293a
Demeter Csaba <nuador@yahoo.com>
2016-11-07 13:40:51 +0200

    ShaderProgram created.

--- Game/Game/GUI.cpp
+++ Game/Game/GUI.cpp
@@ -2,22 +2,26 @@
 
 GUI::GUI() {
 	VertexData2D v1;
-	v1.vertex = vec2(-0.5,-0.5);
+	v1.vertex = vec2(-1, -1);
+	//v1.vertex = vec2(-0.5,-0.5);
 	v1.color = vec3(1, 0, 0);
 	m_Vertices.push_back(v1);
 
 	VertexData2D v2;
-	v2.vertex = vec2(-0.5, 0.5);
+	v2.vertex = vec2(-1, 1);
+	//v2.vertex = vec2(-0.5, 0.5);
 	v2.color = vec3(0, 1, 0);
 	m_Vertices.push_back(v2);
 
 	VertexData2D v3;
-	v3.vertex = vec2(0.5, 0.5);
+	v3.vertex = vec2(1, 1);
+	//v3.vertex = vec2(0.5, 0.5);
 	v3.color = vec3(0, 0, 1);
 	m_Vertices.push_back(v3);
 
 	VertexData2D v4;
-	v4.vertex = vec2(0.5, -0.5);
+	//v4.vertex = vec2(0.5, -0.5);
+	v4.vertex = vec2(1, -1);
 	v4.color = vec3(1, 1, 1);
 	m_Vertices.push_back(v4);
 
--- Game/Game/ShaderProgram.cpp
+++ Game/Game/ShaderProgram.cpp
@@ -5,53 +5,83 @@ ShaderProgram::ShaderProgram(string name):
 {
 	m_ShaderID = glCreateProgram();
 	GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
-	// geometry
+	GLuint geometry = glCreateShader(GL_GEOMETRY_SHADER);
 	GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
 
 	ifstream vertexFile(name + ".vert");
+	if (vertexFile.good()) {
+		string vertSourceString((istreambuf_iterator<char>(vertexFile)), istreambuf_iterator<char>());
+		const char* vertSource = vertSourceString.c_str();
+
+		glShaderSource(vertex, 1, &vertSource, NULL);
+		glCompileShader(vertex);
+
+		GLint result;
+		glGetShaderiv(vertex, GL_COMPILE_STATUS, &result);
+		if (result == GL_FALSE) {
+			GLint length;
+			glGetShaderiv(vertex, GL_INFO_LOG_LENGTH, &length);
+			vector<char> error(length);
+			glGetShaderInfoLog(vertex, length, &length, &error[0]);
+			cout << "Faild to compile vertex shader: " << &error[0] << endl;
+			glDeleteShader(vertex);
+			// Todo: Throw exception
+		}
+
+		glAttachShader(m_ShaderID, vertex);
+	}
+
+	ifstream geometryFile(name + ".geom");
+	if (geometryFile.good()) {
+		string geomSourceString((istreambuf_iterator<char>(geometryFile)), istreambuf_iterator<char>());
+		const char* geomSource = geomSourceString.c_str();
+
+		glShaderSource(geometry, 1, &geomSource, NULL);
+		glCompileShader(geometry);
+
+		GLint result;
+		glGetShaderiv(geometry, GL_COMPILE_STATUS, &result);
+		if (result == GL_FALSE) {
+			GLint length;
+			glGetShaderiv(geometry, GL_INFO_LOG_LENGTH, &length);
+			vector<char> error(length);
+			glGetShaderInfoLog(geometry, length, &length, &error[0]);
+			cout << "Faild to compile geometry shader: " << &error[0] << endl;
+			glDeleteShader(geometry);
+			// Todo: Throw exception
+		}
+
+		glAttachShader(m_ShaderID, geometry);
+	}
+
 	ifstream fragmentFile(name + ".frag");
-	string vertSourceString((istreambuf_iterator<char>(vertexFile)),istreambuf_iterator<char>());
-	string fragSourceString((istreambuf_iterator<char>(fragmentFile)), istreambuf_iterator<char>());
+	if(fragmentFile.good()) {
+		string fragSourceString((istreambuf_iterator<char>(fragmentFile)), istreambuf_iterator<char>());
+		const char* fragSource = fragSourceString.c_str();
 
-	const char* vertSource = vertSourceString.c_str();
-	const char* fragSource = fragSourceString.c_str();
+		glShaderSource(fragment, 1, &fragSource, NULL);
+		glCompileShader(fragment);
 
-	glShaderSource(vertex, 1, &vertSource, NULL);
-	glCompileShader(vertex);
+		GLint result;
+		glGetShaderiv(fragment, GL_COMPILE_STATUS, &result);
+		if (result == GL_FALSE) {
+			GLint length;
+			glGetShaderiv(fragment, GL_INFO_LOG_LENGTH, &length);
+			vector<char> error(length);
+			glGetShaderInfoLog(fragment, length, &length, &error[0]);
+			cout << "Faild to compile fragment shader: " << &error[0] << endl;
+			glDeleteShader(fragment);
+			// Todo: Throw exception
+		}
 
-	GLint result;
-	glGetShaderiv(vertex, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE) {
-		GLint length;
-		glGetShaderiv(vertex, GL_INFO_LOG_LENGTH, &length);
-		vector<char> error(length);
-		glGetShaderInfoLog(vertex, length, &length, &error[0]);
-		cout << "Faild to compile vertex shader: " << &error[0] << endl;
-		glDeleteShader(vertex);
-		// Todo: Throw exception
+		glAttachShader(m_ShaderID, fragment);
 	}
-
-	glShaderSource(fragment, 1, &fragSource, NULL);
-	glCompileShader(fragment);
-
-	glGetShaderiv(fragment, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE) {
-		GLint length;
-		glGetShaderiv(fragment, GL_INFO_LOG_LENGTH, &length);
-		vector<char> error(length);
-		glGetShaderInfoLog(fragment, length, &length, &error[0]);
-		cout << "Faild to compile fragment shader: " << &error[0] << endl;
-		glDeleteShader(fragment);
-		// Todo: Throw exception
-	}
-
-	glAttachShader(m_ShaderID, vertex);
-	glAttachShader(m_ShaderID, fragment);
-
+	
 	glLinkProgram(m_ShaderID);
 	glValidateProgram(m_ShaderID);
 
 	glDeleteShader(vertex);
+	glDeleteShader(geometry);
 	glDeleteShader(fragment);
 }
 
@@ -65,4 +95,39 @@ void ShaderProgram::enable() const {
 
 void ShaderProgram::disable() const {
 	glUseProgram(0);
+}
+
+GLuint ShaderProgram::getUniformLocation(const GLchar* name) {
+	if (m_Uniforms.find(name) == m_Uniforms.end()) {
+		m_Uniforms[name] = glGetUniformLocation(m_ShaderID, name);
+	}
+	return m_Uniforms[name];
+}
+
+void ShaderProgram::setUniform1i(const GLchar* name, int value) {
+	glUniform1i(getUniformLocation(name), value);
+}
+
+void ShaderProgram::setUniformiv(const GLchar* name, int* value, int count) {
+	glUniform1iv(getUniformLocation(name), count, value);
+}
+
+void ShaderProgram::setUniform1f(const GLchar* name, float value) {
+	glUniform1f(getUniformLocation(name), value);
+}
+
+void ShaderProgram::setUniform2f(const GLchar* name, const vec2& vector) {
+	glUniform2f(getUniformLocation(name), vector.x, vector.y);
+}
+
+void ShaderProgram::setUniform3f(const GLchar* name, const vec3& vector) {
+	glUniform3f(getUniformLocation(name), vector.x, vector.y, vector.z);
+}
+
+void ShaderProgram::setUniform4f(const GLchar* name, const vec4& vector) {
+	glUniform4f(getUniformLocation(name), vector.x, vector.y, vector.z, vector.w);
+}
+
+void ShaderProgram::setUniformMat4(const GLchar* name, const mat4& matrix) {
+	glUniformMatrix4fv(getUniformLocation(name), 1, GL_FLOAT, matrix.elements);
 }
